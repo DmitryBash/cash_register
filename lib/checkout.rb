ccd1854280535d24fdb7c74e994d6cd80abf8b94
Batracov Dmitrii <db@batracovs-mbp.home>
2024-01-29 19:52:55 +0100

    adjust Checkout#total to pass tests

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'pricing_rules'
+
 class Checkout
   def initialize
     @basket = []
+    @pricing_rules = PricingRules.new(product_prices)
   end
 
   def add_product(product)
@@ -8,6 +13,22 @@ class Checkout
   end
 
   def total
-    @basket.map(&:price).sum
+    total_price = 0
+    product_counts = Hash.new(0)
+
+    @basket.each { |product| product_counts[product] += 1 }
+    product_counts.each do |product, count|
+      total_price += @pricing_rules.get_price(product, count)
+    end
+
+    total_price.round(2)
+  end
+
+  private
+
+  def product_prices
+    @basket.each_with_object({}) do |product, p_r|
+      p_r[product.code] = product.price
+    end
   end
 end
